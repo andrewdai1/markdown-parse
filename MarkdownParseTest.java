a71692c98c49a9aff444da296cc7abb1c3840c2c
andrewdai1 <97704775+andrewdai1@users.noreply.github.com>
2022-01-27 14:02:13 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,14 +1,41 @@
-//imports java things for the tester to work
+import java.nio.file.Files;
+import java.nio.file.Path;
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
 
-//class of the program
 public class MarkdownParseTest {
-    //labels it as a test?
+        @Test
+       public void addition() {
+                assertEquals(2, 1 + 1);
+    }
+
     @Test
-    //a test method
-    public void addition() {
-        //checks if 1+1=2
-        assertEquals(2, 1 + 1);
+    public void testLinks()  throws IOException {
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://something.com", "some-page.html"),
+        links );
+    }
+
+    @Test
+    public void testLinks2()  throws IOException {
+        Path fileName = Path.of("test-file2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("google.com", "youtube.com", "gmail.com"),
+        links );
+    }
+
+    @Test
+    public void testLinks3()  throws IOException {
+        Path fileName = Path.of("test-file3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of(),
+        links );
     }
 }
