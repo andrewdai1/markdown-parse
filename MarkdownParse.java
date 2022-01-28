351ec96dcc902345b186a12cf635858393578789
andrewdai1 <97704775+andrewdai1@users.noreply.github.com>
2022-01-28 12:45:54 -0800

    Lab 2 update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,11 +19,11 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            for(int i = 0; i <= markdown.length(); i++){
-                if (closeParen < markdown.length()) {
-                    closeParen = markdown.indexOf(")", closeParen + i);
-                }
-            }
+            // for(int i = 0; i <= markdown.length(); i++){
+            //     if (closeParen < markdown.length()) {
+            //         closeParen = markdown.indexOf(")", closeParen + i);
+            //     }
+            // }
             
 
             toReturn.add(markdown.substring(openParen + 1, closeParen));
@@ -37,8 +37,6 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
-        
-        System.out.println("Test");
-    }
+        }
 }
 
